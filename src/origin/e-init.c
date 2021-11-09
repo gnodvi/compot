8441793d8f178ad7d53fe556ec4e62cc054b82b6
gnodvi meranov <gnodvimeranov@mail.ru>
2021-11-09 15:45:33 +0300

    start graf from c library

---
+++ src/origin/e-init.c
@@ -0,0 +1,407 @@
+// -*-  mode: c    ; coding: koi8   -*- ----------------------------------------
+
+//------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>   
+#include <string.h>
+#include <math.h> 
+
+#ifdef _JIM
+#include <jim.h>
+#else
+#include <tcl.h>
+#endif
+
+//#include <tcl.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+//  #include "os.h"
+//  #include "capture.h"
+//  #include "misc.h"
+
+//------------------------------------------------------------------------------
+
+//#include "r-main.h"
+#include "e-0000.h"
+
+//#define NO_CONST 1   ?????????
+ 
+#ifdef _JIM
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define Gen_Interp Jim_Interp
+#define Gen_CreateCommand Jim_CreateCommand
+#define GEN_OK JIM_OK 
+#define GEN_ERROR JIM_ERR 
+#define Gen_PkgProvideEx Jim_PackageProvide
+#define Gen_SetResult Jim_SetResultString 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#else
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define Gen_Interp        Tcl_Interp
+#define Gen_CreateCommand Tcl_CreateCommand
+#define GEN_OK            TCL_OK 
+#define GEN_ERROR         TCL_ERROR 
+#define Gen_PkgProvideEx  Tcl_PkgProvideEx
+#define Gen_SetResult     Tcl_SetResult
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#endif
+
+
+//------------------------------------------------------------------------------
+#ifdef _0
+//------------------------------------------------------------------------------
+#ifdef _JIM
+static int  
+knuth_random_seed (/* ClientData clientData, */ Gen_Interp *interp,
+		   int argc, Jim_Obj *const *argv) 
+#else
+int  
+knuth_random_seed (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+
+  //if (argc != 2 && argc != 3) {
+  //  Tcl_AppendResult(interp, "wrong # args: should be \"",
+  //                   argv[0], " command ?varName?\"", NULL);
+  //  return TCL_ERROR;
+  //}
+  
+  const char *str;
+  int len;
+
+#ifdef _JIM
+  str = Jim_GetString(argv[1], &len);
+#else
+  str = argv[1];
+#endif
+
+  //int seed = atoi (argv[1]);
+  int seed = atoi (str);
+
+  usrand (seed);
+
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifdef _JIM
+static int  
+knuth_random_rand (/* ClientData clientData, */ Gen_Interp *interp,
+		   int argc, Jim_Obj *const *argv) 
+#else
+int  
+knuth_random_rand (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+  char buf[80];
+
+  int r = urand ();
+
+  sprintf (buf,    "%10u", r);
+  //fprintf (stderr, "%10u \n", r);
+
+  
+#ifdef _JIM
+  int len = /* 10 */ strlen (buf);
+  Jim_SetResultString (interp, buf, /* NULL */ len);
+#else
+  Tcl_SetResult (interp, buf, /* NULL */ 0); //??
+#endif
+  
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifdef _JIM
+int  
+knuth_random_randint (/* ClientData clientData, */ Gen_Interp *interp,
+		      int argc, Jim_Obj *const *argv) 
+#else
+int  
+knuth_random_randint (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+  const char *str;
+  int len;
+
+#ifdef _JIM
+  str = Jim_GetString(argv[1], &len);
+#else
+  str = argv[1];
+#endif
+
+  //int upto = 10;
+  int upto = atoi (/* argv[1] */ str);
+
+  char buf[80];
+
+  int r = randint (upto);
+
+  sprintf (buf, "%10u", r);
+  //sprintf (buf, "%d\n", r);
+
+  //Gen_SetResult (interp, buf, /* NULL */ 10);
+  
+#ifdef _JIM
+  /* int */ len = 10;
+  Jim_SetResultString (interp, buf, /* NULL */ len);
+#else
+  Tcl_SetResult (interp, buf, /* NULL */ 0); //??
+#endif
+
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifdef _JIM
+int  
+knuth_random_randreal (/* ClientData clientData, */ Gen_Interp *interp,
+		      int argc, Jim_Obj *const *argv) 
+#else
+int  
+knuth_random_randreal (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+  const char *str;
+  int len;
+
+#ifdef _JIM
+  str = Jim_GetString(argv[1], &len);
+#else
+  str = argv[1];
+#endif
+
+  //int upto = 10;
+  //int upto = atoi (/* argv[1] */ str);
+  double upto = atof (/* argv[1] */ str);
+
+  char buf[80];
+
+  //int r = randint (upto);
+  double r = randreal (upto);
+
+  //sprintf (buf, "%10u", r);
+  sprintf (buf, "%10.3f", r);
+  //sprintf (buf, "%d\n", r);
+
+  //Gen_SetResult (interp, buf, /* NULL */ 10);
+  
+#ifdef _JIM
+  /* int */ len = 10;
+  Jim_SetResultString (interp, buf, /* NULL */ len);
+#else
+  Tcl_SetResult (interp, buf, /* NULL */ 0); //??
+#endif
+
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#endif  // #ifdef _0
+
+
+
+//------------------------------------------------------------------------------
+#ifdef _JIM
+static int  
+mg_test_01 (/* ClientData clientData, */ Gen_Interp *interp,
+		   int argc, Jim_Obj *const *argv) 
+#else
+int  
+mg_test_01 (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+
+  test_01 ();
+  
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifdef _JIM
+static int  
+mg_graf_create (Gen_Interp *interp, int argc, Jim_Obj *const *argv) 
+#else
+int  
+mg_graf_create (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+
+  const char *str;
+  int len;
+  char buf[80];
+
+
+#ifdef _JIM
+  str = Jim_GetString(argv[1], &len);
+#else
+  str = argv[1];
+#endif
+
+  int  nn = atoi (str);
+
+  GRAF *gr = GrafCreate (nn);
+ 
+  printf ("mg_graf_create: gr = %p \n", (void*) gr);
+ 
+  sprintf (buf, "%p", (void*) gr);
+
+  printf ("mg_graf_create: buf = %s \n", buf);
+   
+#ifdef _JIM
+  //len = 10;
+  Jim_SetResultString (interp, buf, /* NULL */ strlen (buf));
+#else
+  Tcl_SetResult (interp, buf, /* NULL */ 0); //??
+#endif
+
+
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifdef _JIM
+static int  
+mg_graf_print (Gen_Interp *interp, int argc, Jim_Obj *const *argv) 
+#else
+int  
+mg_graf_print (ClientData clientData, Gen_Interp *interp, int argc, char **argv) 
+#endif
+{
+
+  const char *str; int len;
+  char buf[80];
+
+
+#ifdef _JIM
+  str = Jim_GetString (argv[1], &len);
+#else
+  str = argv[1];
+#endif
+
+  printf ("str = %s \n", str);
+
+  GRAF *gr;
+  sscanf (str, "%p", (void*) &gr);
+
+  printf ("mg_graf_print: gr = %p \n", (void*) gr);
+
+
+  GrafPrint (gr);
+  
+
+  return GEN_OK;
+}
+//------------------------------------------------------------------------------
+#ifndef _JIM
+int 
+Metagraph_Init (Gen_Interp *interp) 
+#else
+int 
+Jim_e_jimInit (Gen_Interp *interp) 
+#endif
+{
+
+  // инициализация интерфейса (обходился и без нее ?)
+  // stub library - использование библиотеки-заглушки
+  //
+  char *version = "8.5";  // минимальная версия
+
+#ifndef _JIM
+  if (Tcl_InitStubs (interp, version, 0) == NULL) {
+    return GEN_ERROR;
+  }
+#endif
+
+
+
+  Gen_CreateCommand (interp, "mg_test_01", mg_test_01,
+                     /* (ClientData) */ NULL, 
+                     /* (Tcl_CmdDeleteProc *) */ NULL);
+
+  Gen_CreateCommand (interp, "mg_graf_create", mg_graf_create,
+                     /* (ClientData) */ NULL, 
+                     /* (Tcl_CmdDeleteProc *) */ NULL);
+
+  Gen_CreateCommand (interp, "mg_graf_print", mg_graf_print,
+                     /* (ClientData) */ NULL, 
+                     /* (Tcl_CmdDeleteProc *) */ NULL);
+
+
+  // можно подготовить реализацию пакета, тогда можно будет
+  // вызывать как package requare 
+
+  Gen_PkgProvideEx (interp, "metagraph", "1.1", /* NULL */ 0);
+
+  return GEN_OK;  
+}
+//------------------------------------------------------------------------------
+// load a_capt.so Capture
+// 
+// package require capture
+// 
+//------------------------------------------------------------------------------
+//int main () 
+//{
+
+//   puts ("");
+//   printf ("@@@@@@@@@@@@@@@@ \n");
+//   puts ("");
+
+//   Tcl_Interp *interp;
+
+//   interp = Tcl_CreateInterp ();
+
+//   int ret = Tcl_Eval (interp, "exec ls -all");
+
+//   if (ret == TCL_OK) {
+//     puts ("OK!............");
+//   } else {
+//     puts ("NOT OK!............");
+//   }
+
+//   puts ("");
+
+//   const char *str;
+
+//   str = Tcl_GetStringResult (interp);
+//   printf ("str1 = %s \n", str);
+
+//   //-----------------------------------------------------------
+//   Tcl_Obj *resobj = Tcl_GetObjResult (interp);
+
+//   str = Tcl_GetString (resobj);
+//   printf ("str2 = %s \n", str);
+
+//   int l; //intPtr - куда положим результат (длину списка)
+//   ret = Tcl_ListObjLength (interp, resobj, &l);
+
+//   if (ret == TCL_OK) {
+//     puts ("OK!............");
+//   } else {
+//     puts ("NOT OK!............");
+//   }
+
+//   puts ("");
+//   printf ("l = %d", l);
+//   puts ("");
+
+//   Tcl_Obj  **objvPtr; // where Tcl_ListObjGetElements stores a 
+//   // pointer to an array of pointers to the element objects of listPtr. 
+//   ret = Tcl_ListObjGetElements (interp, resobj, &l, &objvPtr);
+
+//   if (ret == TCL_OK) {
+//     puts ("OK!............");
+//   } else {
+//     puts ("NOT OK!............");
+//   }
+
+//   int i;
+//   for (i = 0; i < l; i++) {
+//     str = Tcl_GetString (objvPtr[i]);
+//     printf ("i= %d  str = %s \n", i, str);
+//   }
+
+//   puts ("");
+//}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
---
+++ src/origin/e_test.c
@@ -0,0 +1,184 @@
+// -*-  mode: c    ; coding: koi8   -*- ----------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <getopt.h>    /* for getopt_long; POSIX standard getopt is in unistd.h */
+
+
+#include <sys/time.h>
+#include <sys/param.h>
+#include <sys/timeb.h>
+
+#include <semaphore.h>
+#include <errno.h>
+#include <unistd.h>
+#include <limits.h>
+#include <libgen.h>
+#include <sys/types.h>
+#include <stdint.h>
+
+/*-----------------------------------------------------------------------------*/ 
+
+
+#include "e-0000.h"
+
+//#include "e-graf.h"
+
+
+/*-----------------------------------------------------------------------------*/ 
+int
+main (int argc, char *argv[])
+{
+
+  int test_num   = 0;
+
+  //get_options (argc, argv, 
+  //           NULL, NULL, 
+  //           /* &interval_ms */ NULL, /* &count_max */ NULL, 
+
+  //           NULL, NULL, &test_num, NULL);
+
+  int is_win = 0;
+  int is_gnu = 0;
+
+  //---------------------------------------------
+
+
+  optind = 1; // чтобы бы повторном (вложенном вызове) начать с начала 
+
+  static struct option long_options[] = {
+    /*   NAME       ARGUMENT           FLAG  SHORTNAME */
+    {"verbose", no_argument,       NULL,   0},
+    {"create",  required_argument, NULL, 'c'},
+    {"win",     no_argument, NULL,   0},
+    {"gnu",     no_argument, NULL,   0},
+    {"test",    required_argument, NULL,   0},
+    {"loop0",   no_argument,       NULL,   0},
+    {"loop1",   no_argument,       NULL,   0},
+    {NULL,      0,                 NULL,   0}
+  };
+  int option_index = 0;
+  
+
+  // перебираем все параметры:
+  // (в QNX эти параметры должны идти первыми)
+
+  DAO_LOOP daoloop = dao_loop0;
+  char c;
+  const char *name;
+
+  while ((c = getopt_long (argc, argv, "uha:p:i:n:o:vt:",
+                      long_options, &option_index)) != -1) { 
+
+    //fprintf (stderr, "get_options: optind = %d   c = %c \n", optind, c);
+
+    switch (c) {
+
+    case 0: // обработка длинных опций
+
+      name = long_options[option_index].name;
+
+      printf ("option %s", name);
+      if (optarg)
+        printf (" with arg %s", optarg);
+      printf ("\n");
+
+      if (! strcmp (name, "win"))   is_win  = 1;
+      if (! strcmp (name, "gnu"))   is_gnu  = 1;
+
+      if (! strcmp (name, "loop0")) daoloop = dao_loop0;
+      if (! strcmp (name, "loop1")) daoloop = dao_loop1;
+
+      break;
+      
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    case 't': 
+      test_num = atoi (optarg); 
+     break;
+
+    case 'h': 
+      get_options_print_help (); 
+      break;
+
+      //default:      
+      // exit( EXIT_FAILURE ); // сюда невозможно попасть!
+    }
+  }
+
+
+  YT_PLOT *plot;
+  
+  switch (test_num) {
+    
+  case 0:
+    daoloop (is_win, is_gnu); // вызываем уже заданную функцию
+    break;
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // а здесь вспомогательные тесты, например по графам 
+
+  case 1:
+    test_01 ();
+    break;
+
+  case 11:
+    test_11 ();
+    break;
+
+  case 2:
+    test_00 (4);
+    break;
+
+
+    //case 3:
+    //dao_loop0 (/* argc, argv */is_win, is_gnu);
+    //break;
+
+  case 4:
+    plot = make_plot_test (100, 0, 2*YPI);
+    plot_print (plot, FALSE, TRUE);
+    break;
+
+  default:
+    break;
+
+  }
+
+  printf ("\n");
+
+  //return 1;
+  return (0); // Tcl's exec treats any non-zero exit status as an exceptional 
+  // situation. 
+}
+//------------------------------------------------------------------------------
+
+// time e_meta -t3
+
+//------------------------------------------------------------------------------
+
+// cl g~.cl GRAF test_01      
+// c_graf -t1
+
+// sl g~.cl GRAF test_11 
+
+// cl g~.cl NETS amat_03 
+// sl g~.cl META tao_loop0 500 1  1706 0 
+// sl g~.cl META tao_loop1 200 2 3  1706 0
+
+
+// g~.tl GRAF test_01 
+// g~.tl GRAF test_11 
+// g~.tl GRAF test_99 
+// g~.tl META tao_loop0 5 1    1706 0 
+// g~.tl META tao_loop1 3  2 3  1706 0 
+// g~.tl META test_00 
+// g~.tl META test_05 
+// g~.tl NETS amat_03 
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
