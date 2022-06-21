d853525552a0b5a00d18938c363d350a23d6b339
Gnodvi Meranov <gnodvimeranov@mail.ru>
2022-06-21 13:38:41 +0300

    rename origin/l to origin/r

--- src/fun-gp/r_code.cc
+++ src/fun-gp/r_code.cc
@@ -114,7 +114,7 @@ D. Backspace
 
 using namespace std;
 
-#define FILENAME "l_code.in"
+#define FILENAME "r_code.in"
 
 //------------------------------------------------------------------------------
 
