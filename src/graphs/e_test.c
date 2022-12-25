73cff6c276b0401ecd314aed3cc254d3e2fa938d
gnodvi meranov <gnodvimeranov@mail.ru>
2022-12-25 20:15:46 +0300

    add IGRAPH/examples

--- src/graphs/e_test.c
+++ src/graphs/e_test.c
@@ -10,83 +10,30 @@
 #include <getopt.h>    /* for getopt_long; POSIX standard getopt is in unistd.h */
 
 
-/* #include <sys/time.h> */
-/* #include <sys/param.h> */
-/* #include <sys/timeb.h> */
+#include <sys/time.h>
+#include <sys/param.h>
+#include <sys/timeb.h>
 
-/* #include <semaphore.h> */
-/* #include <errno.h> */
-/* #include <unistd.h> */
-/* #include <limits.h> */
-/* #include <libgen.h> */
-/* #include <sys/types.h> */
-/* #include <stdint.h> */
-
-#include <igraph.h>
-
+#include <semaphore.h>
+#include <errno.h>
+#include <unistd.h>
+#include <limits.h>
+#include <libgen.h>
+#include <sys/types.h>
+#include <stdint.h>
 
 /*-----------------------------------------------------------------------------*/ 
 
-//#include "e-0000.h"
+#include "e-0000.h"
 
 //#include "e-graf.h"
 
-/*-----------------------------------------------------------------------------*/ 
-void
-test_01 ()
-{
-
-  //GRAF *pg;
-  //int   s, t, nn;
-  //double  r, d;
-  //int     num_r, num_d;
-
-  igraph_t graph;
-
-  printf ("\n");
-  printf (".... test_01 .................... \n");
-
-  //pg = GrafCreate (4);
-  
-  //GrafInit (pg, YRAND, 0.0, 0.0, YRAND, NOT, NOT);
-  
-  //EDGE (pg, 0, 1) = 1;
-  //EDGE (pg, 1, 2) = 1;
-  //EDGE (pg, 2, 3) = 1;
-  //EDGE (pg, 0, 3) = 3.1 /* 2.9 */;
-  
-
-  igraph_integer_t edges[] = { 0,1, 1,2, 2,3, 0,3 };
-
-  //VECTOR(edges)[0] = 1;
-
-  //igraph_add_edges (&graph, &edges, NULL);
-
-
-  //s = 0;
-  //t = 3;
-  
-  //printf ("\n");
-  //GrafPrint (pg);
-
-  //GrafSmin (pg, s, t, NULL);
-  //printf ("\n");
-
-  //GrafMetrica (pg, &r, &d, &num_r, &num_d);
-
-  //printf ("r = %.1f  num_r = %d \n", r, num_r);
-  //printf ("d = %.1f  num_d = %d \n", d, num_d);
-
-  printf ("\n");
-
-  return;
-}
 /*-----------------------------------------------------------------------------*/ 
 int
 main (int argc, char *argv[])
 {
 
-  int test_num   = 1; // test_01
+  int test_num   = 0;
 
   //get_options (argc, argv, 
   //           NULL, NULL, 
@@ -94,8 +41,8 @@ main (int argc, char *argv[])
 
   //           NULL, NULL, &test_num, NULL);
 
-  //int is_win = 0;
-  //int is_gnu = 0;
+  int is_win = 0;
+  int is_gnu = 0;
 
   //---------------------------------------------
 
@@ -103,13 +50,14 @@ main (int argc, char *argv[])
   optind = 1; // чтобы бы повторном (вложенном вызове) начать с начала 
 
   static struct option long_options[] = {
-/*     {"verbose", no_argument,       NULL,   0}, */
-/*     {"create",  required_argument, NULL, 'c'}, */
-/*     {"win",     no_argument, NULL,   0}, */
-/*     {"gnu",     no_argument, NULL,   0}, */
-/*     {"test",    required_argument, NULL,   0}, */
-/*     {"loop0",   no_argument,       NULL,   0}, */
-/*     {"loop1",   no_argument,       NULL,   0}, */
+    /*   NAME       ARGUMENT           FLAG  SHORTNAME */
+    {"verbose", no_argument,       NULL,   0},
+    {"create",  required_argument, NULL, 'c'},
+    {"win",     no_argument, NULL,   0},
+    {"gnu",     no_argument, NULL,   0},
+    {"test",    required_argument, NULL,   0},
+    {"loop0",   no_argument,       NULL,   0},
+    {"loop1",   no_argument,       NULL,   0},
     {NULL,      0,                 NULL,   0}
   };
   int option_index = 0;
@@ -118,7 +66,7 @@ main (int argc, char *argv[])
   // перебираем все параметры:
   // (в QNX эти параметры должны идти первыми)
 
-  //DAO_LOOP daoloop = dao_loop0;
+  DAO_LOOP daoloop = dao_loop0;
   char c;
   const char *name;
 
@@ -138,11 +86,11 @@ main (int argc, char *argv[])
         printf (" with arg %s", optarg);
       printf ("\n");
 
-/*       if (! strcmp (name, "win"))   is_win  = 1; */
-/*       if (! strcmp (name, "gnu"))   is_gnu  = 1; */
+      if (! strcmp (name, "win"))   is_win  = 1;
+      if (! strcmp (name, "gnu"))   is_gnu  = 1;
 
-/*       if (! strcmp (name, "loop0")) daoloop = dao_loop0; */
-/*       if (! strcmp (name, "loop1")) daoloop = dao_loop1; */
+      if (! strcmp (name, "loop0")) daoloop = dao_loop0;
+      if (! strcmp (name, "loop1")) daoloop = dao_loop1;
 
       break;
       
@@ -161,12 +109,12 @@ main (int argc, char *argv[])
   }
 
 
-  //YT_PLOT *plot;
+  YT_PLOT *plot;
   
   switch (test_num) {
     
   case 0:
-    //daoloop (is_win, is_gnu); // вызываем уже заданную функцию
+    daoloop (is_win, is_gnu); // вызываем уже заданную функцию
     break;
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -177,11 +125,11 @@ main (int argc, char *argv[])
     break;
 
   case 11:
-    //test_11 ();
+    test_11 ();
     break;
 
   case 2:
-    //test_00 (4);
+    test_00 (4);
     break;
 
 
@@ -189,10 +137,10 @@ main (int argc, char *argv[])
     //dao_loop0 (/* argc, argv */is_win, is_gnu);
     //break;
 
-    //case 4:
-    //plot = make_plot_test (100, 0, 2*YPI);
-    //plot_print (plot, FALSE, TRUE);
-    //break;
+  case 4:
+    plot = make_plot_test (100, 0, 2*YPI);
+    plot_print (plot, FALSE, TRUE);
+    break;
 
   default:
     break;
