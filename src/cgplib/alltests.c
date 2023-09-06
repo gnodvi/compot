496258a1a7e63d858baee0ce2da74c186004688d
Gnodvi Meranov <gnodvimeranov@mail.ru>
2023-09-06 04:42:18 +0300

    cgplib refact

--- src/cgplib/alltests.c
+++ src/cgplib/alltests.c
@@ -429,6 +429,445 @@ int test_customReproductionScheme (int argc, char **argv) {
 }
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+void tournament (struct parameters *params, struct chromosome **parents, 
+                struct chromosome **candidateChromos, int numParents, int numCandidateChromos){
+
+  int i;
+  
+  struct chromosome *candidateA;
+  struct chromosome *candidateB;
+  
+  srand (2020);  //gnodvi
+
+  for(i=0; i<numParents; i++){
+    
+    candidateA = candidateChromos[rand() % numCandidateChromos];
+    candidateB = candidateChromos[rand() % numCandidateChromos];
+    
+    if(getChromosomeFitness(candidateA) <= getChromosomeFitness(candidateB)){
+      copyChromosome(parents[i], candidateA);
+    }
+    else{
+      copyChromosome(parents[i], candidateB);
+    }		
+  }
+
+}
+//------------------------------------------------------------------------------
+int test_customSelectionScheme (int argc, char **argv){
+	
+  struct parameters *params = NULL;
+  struct dataSet *trainingData = NULL;
+  struct chromosome *chromo = NULL;
+	
+  int numInputs = 1;
+  int numNodes = 50;
+  int numOutputs = 1;
+  int arity = 2;
+	
+  double targetFitness = 0.1;
+  int updateFrequency = 1000;
+  int numGens = 10000;
+	
+  params = initialiseParameters(numInputs, numNodes, numOutputs, arity);
+	  
+  //---------------------------------
+  setRandomNumberSeed (2021);
+  //---------------------------------
+
+  addNodeFunction(params, "add,sub,mul,div,sin");
+  
+  setTargetFitness(params, targetFitness);
+  
+  setUpdateFrequency(params, updateFrequency); 
+  
+  setCustomSelectionScheme(params, tournament, "tournament");
+  
+  trainingData = initialiseDataSetFromFile("./dataSets/symbolic.data");
+  
+  chromo = runCGP (params, trainingData, numGens);
+  
+  freeChromosome(chromo);
+  freeDataSet(trainingData);
+  freeParameters(params);
+  
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+#define POPULATIONSIZE 5
+#define NUMINPUTS 1
+#define NUMNODES 8
+#define NUMOUTPUTS 1
+#define ARITY 2
+  
+//------------------------------------------------------------------------------
+int test_manipulatingChromosomes (int argc, char **argv) {
+
+  struct parameters *params = NULL;
+  struct chromosome *chromoA = NULL;
+  struct chromosome *chromoB = NULL;
+  struct chromosome *chromoC = NULL;
+  struct dataSet *trainingData = NULL;
+  
+  double testInputs[NUMINPUTS];
+  
+  params = initialiseParameters (NUMINPUTS, NUMNODES, NUMOUTPUTS, ARITY);
+  
+  //---------------------------------
+  setRandomNumberSeed (2021);
+  //---------------------------------
+
+  addNodeFunction(params, "add,sub,mul,sq,cube,sin");
+
+  trainingData = initialiseDataSetFromFile("./dataSets/symbolic.data");
+
+  chromoA = initialiseChromosome(params);
+  chromoB = initialiseChromosome(params);
+
+  setChromosomeFitness(params, chromoA, trainingData);
+
+  mutateChromosome(params, chromoA);
+
+  copyChromosome(chromoB, chromoA);
+
+  removeInactiveNodes(chromoB);
+
+  printf("chromoA with inactive nodes.\n");
+  printChromosome(chromoA, 0);
+
+  printf("chromoB without inactive nodes.\n");
+  printChromosome(chromoB, 0);
+
+  saveChromosome(chromoB, "chromoB.chromo");
+
+  chromoC = initialiseChromosomeFromFile("chromoB.chromo");
+
+  testInputs[0] = 3;
+
+  executeChromosome(chromoC, testInputs);
+
+  printf("Applied input: %f\n", testInputs[0]);
+  printf("Generated output: %f\n", getChromosomeOutput(chromoC, 0));
+
+  freeChromosome(chromoA);
+  freeChromosome(chromoB);
+  freeChromosome(chromoC);
+  freeDataSet(trainingData);
+  freeParameters(params);
+
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+/*
+	Custom fitness function which just does some work and returns 10.
+*/
+double fitnessFunction 
+  (struct parameters *params, struct chromosome *chromo, struct dataSet *data) {
+
+  int i;
+  double inputs[1] = {0.5};
+  int numExec = 10000;
+
+  for (i = 0; i < numExec; i++) {
+    executeChromosome(chromo, inputs);
+  }
+
+  return 10;
+}
+//------------------------------------------------------------------------------
+int test_multipleThreads (int argc, char **argv) {
+
+  time_t timeStart, timeEnd;
+  double singleThreadTime, multipleThreadTime;
+  double seedUp;
+
+  struct parameters *params = NULL;
+
+  int numInputs = 1;
+  int numNodes = 100;
+  int numOutputs = 1;
+  int arity = 2;
+
+  // mgena 
+  //int gens = 100;
+  //int runs = 10;
+
+  int gens = 30;
+  int runs = 4;
+
+  params = initialiseParameters (numInputs, numNodes, numOutputs, arity);
+
+  //---------------------------------
+  setRandomNumberSeed (2020);
+  //---------------------------------
+
+  setRandomNumberSeed(123456789);
+  addNodeFunction(params, "add,sub,mul,div,sin");
+  setMutationRate(params, 1.0);
+  setCustomFitnessFunction(params, fitnessFunction, "FF");
+
+  setNumThreads(params, 1);
+  timeStart = time(NULL);
+  repeatCGP(params, NULL, gens, runs);
+  timeEnd = time(NULL);
+  singleThreadTime = difftime(timeEnd, timeStart);
+
+  setNumThreads(params, 4);
+  timeStart = time(NULL);
+  repeatCGP(params, NULL, gens, runs);
+  timeEnd = time(NULL);
+  multipleThreadTime = difftime(timeEnd, timeStart);
+
+  seedUp = 100 * (singleThreadTime - multipleThreadTime) / multipleThreadTime;
+
+  fprintf (stderr, "Single thread time: %.f seconds\n", singleThreadTime);
+  fprintf (stderr, "Multiple thread time: %.f seconds\n", multipleThreadTime);
+  fprintf (stderr, "Speed up = %.f%%\n", seedUp);
+
+  freeParameters(params);
+
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+double sinWave(struct parameters *params, struct chromosome *chromo, struct dataSet *data) {
+
+  double i;
+
+  double error = 0;
+  double range = 6;
+  double stepSize = 0.5;
+
+  double inputs[1];
+
+  for (i = 0; i < range; i += stepSize) {
+
+    inputs[0] = i;
+
+    executeChromosome(chromo, inputs);
+
+    error += fabs(getChromosomeOutput(chromo, 0) - sin(i));
+  }
+
+  return error;
+}
+//------------------------------------------------------------------------------
+int test_neuroEvolution (int argc, char **argv) {
+
+  struct parameters *params = NULL;
+  struct chromosome *chromo = NULL;
+  
+  int numInputs = 1;
+  int numNodes = 20;
+  int numOutputs = 1;
+  int nodeArity = 5;
+  
+  int numGens = 25000;
+  double targetFitness = 0.5;
+  int updateFrequency = 500;
+  
+  double weightRange = 5;
+  
+  params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
+  
+  setRandomNumberSeed(123456789);
+  
+  setTargetFitness(params, targetFitness);
+  
+  setUpdateFrequency(params, updateFrequency);
+  
+  setConnectionWeightRange(params, weightRange);
+  
+  setCustomFitnessFunction(params, sinWave, "sinWave");
+  
+  addNodeFunction(params, "tanh,softsign");
+  
+  chromo = runCGP(params, NULL, numGens);
+  
+  printChromosome(chromo, 1);
+  
+  freeChromosome(chromo);
+  freeParameters(params);
+  
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+int test_recurrentConnections (int argc, char **argv) {
+
+  struct parameters *params = NULL;
+  struct dataSet *trainingData = NULL;
+  struct chromosome *chromo = NULL;
+
+  int numInputs = 1;
+  int numNodes = 15;
+  int numOutputs = 1;
+  int nodeArity = 2;
+
+  int numGens = 100000;
+  int updateFrequency = 500;
+
+  double recurrentConnectionProbability = 0.10;
+
+  params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
+  
+  //---------------------------------
+  //setRandomNumberSeed (1999);
+  setRandomNumberSeed (2003);
+  //---------------------------------
+
+  addNodeFunction(params, "add,sub,mul,div");
+
+  setUpdateFrequency(params, updateFrequency);
+
+  setRecurrentConnectionProbability(params, recurrentConnectionProbability);
+
+  printParameters(params);
+
+  trainingData = initialiseDataSetFromFile("./dataSets/fibonacci.data");
+
+  chromo = runCGP(params, trainingData, numGens);
+
+  printChromosome(chromo, 0);
+
+  freeDataSet(trainingData);
+  freeChromosome(chromo);
+  freeParameters(params);
+
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+int test_visualization (int argc, char **argv) {
+
+  struct parameters *params = NULL;
+  struct chromosome *chromo = NULL;
+  
+  int numInputs = 1;
+  int numNodes = 8;
+  int numOutputs = 1;
+  int nodeArity = 2;
+  
+  params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
+  
+  //---------------------------------
+  setRandomNumberSeed (2021);
+  //---------------------------------
+
+  addNodeFunction(params, "add,sub,mul,div,sin");
+  
+  chromo = initialiseChromosome(params);
+  
+  printChromosome(chromo, 0);
+  saveChromosomeDot(chromo, 0, "chromo.dot");
+  saveChromosomeLatex(chromo, 0, "chromo.tex");
+  
+  freeChromosome(chromo);
+  freeParameters(params);
+  
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+#define POPULATIONSIZE 5
+//#define NUMINPUTS 3
+
+//#define NUMNODES 30
+#define NUMOUTPUTS 1
+#define ARITY 2
+
+//------------------------------------------------------------------------------
+int test_customES (int argc, char **argv) {
+
+	
+  int i, gen;
+	
+  struct parameters *params = NULL;
+  struct chromosome *population[POPULATIONSIZE]; 
+  struct chromosome *fittestChromosome = NULL;
+  struct dataSet *trainingData = NULL;
+		
+  double targetFitness = 0;
+  int maxGens = 10000;
+				
+  params = initialiseParameters (/* NUMINPUTS */ 3, /* NUMNODES */ 30, NUMOUTPUTS, ARITY);
+  
+  //---------------------------------
+  setRandomNumberSeed (2021);
+  //---------------------------------
+	
+  addNodeFunction(params, "or,nor,and,nand");
+  /*setTargetFitness(params, targetFitness);*/
+  setMutationType(params, "probabilistic");
+  setMutationRate(params, 0.08);
+	
+  trainingData = initialiseDataSetFromFile("./dataSets/parity3bit.data");
+	
+  for(i=0; i<POPULATIONSIZE; i++){
+    population[i] = initialiseChromosome(params);
+  }
+	
+  fittestChromosome = initialiseChromosome(params);
+	
+  /* for the number of allowed generations*/
+  for(gen=0; gen<maxGens; gen++){
+		
+    /* set the fitnesses of the population of chromosomes*/
+    for(i=0; i<POPULATIONSIZE; i++){
+      setChromosomeFitness(params, population[i], trainingData);
+    }
+		
+    /* copy over the last chromosome to fittestChromosome*/
+    copyChromosome(fittestChromosome, population[POPULATIONSIZE - 1]);
+		
+    /* for all chromosomes except the last*/
+    for(i=0; i<POPULATIONSIZE-1; i++){
+			
+      /* copy ith chromosome to fittestChromosome if fitter*/
+      if(getChromosomeFitness(population[i]) < getChromosomeFitness(fittestChromosome)){
+        copyChromosome(fittestChromosome, population[i]);
+      }
+    }
+				
+    /* termination condition*/
+    if(getChromosomeFitness(fittestChromosome) <= targetFitness){
+      break;
+    }
+				
+    /* set the first member of the population to be the fittest chromosome*/
+    copyChromosome (population[0], fittestChromosome);
+		
+    /* set remaining member of the population to be mutations of the
+      fittest chromosome*/
+    for(i=1; i<POPULATIONSIZE; i++){
+			
+      copyChromosome (population[i], fittestChromosome);
+      mutateChromosome (params, population[i]);
+    }
+  }
+	
+  printf("gen\tfitness\n");
+  printf("%d\t%f\n", gen, getChromosomeFitness (fittestChromosome));
+	
+	
+	
+  for(i=0; i<POPULATIONSIZE; i++){
+    freeChromosome (population[i]);
+  }
+	
+  freeChromosome (fittestChromosome);
+  freeDataSet (trainingData);
+  freeParameters (params);
+	
+  return 0;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 int main (int argc, char **argv) {
 
   int  ret = 0;
@@ -446,6 +885,13 @@ int main (int argc, char **argv) {
   else if (! strcmp (buf, "customFitnessFunction")) ret = test_customFitnessFunction (argc, argv);
   else if (! strcmp (buf, "customNodeFunction"))    ret = test_customNodeFunction (argc, argv);
   else if (! strcmp (buf, "customReproductionScheme")) ret = test_customReproductionScheme (argc, argv);
+  else if (! strcmp (buf, "customSelectionScheme"))    ret = test_customSelectionScheme (argc, argv);
+  else if (! strcmp (buf, "manipulatingChromosomes"))  ret = test_manipulatingChromosomes (argc, argv);
+  else if (! strcmp (buf, "manipulatingChromosomes"))  ret = test_multipleThreads (argc, argv);
+  else if (! strcmp (buf, "neuroEvolution")) ret = test_neuroEvolution (argc, argv);
+  else if (! strcmp (buf, "recurrentConnections")) ret = test_recurrentConnections (argc, argv);
+  else if (! strcmp (buf, "visualization")) ret = test_visualization (argc, argv);
+  else if (! strcmp (buf, "customES"))      ret = test_customES (argc, argv);
   else {  
     printf ("\nERROR option -t = %s \n\n", buf);
   }
